0e0db790c6916a4a41b72ae32a39794ef84b5706
Beatriz Vieira <beatrizteixeira_2008@icloud.com>
2026-09-24 12:53:34 -0300

    adiciona storages

--- setup/settings.py
+++ setup/settings.py
@@ -51,6 +51,15 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 ROOT_URLCONF = 'setup.urls'
 
 TEMPLATES = [
